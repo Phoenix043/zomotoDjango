634edbb883a97c7576064eb6f73ded1b21c47d31
Phoenix043 <132455515+Phoenix043@users.noreply.github.com>
2023-08-10 20:03:48 +0530

    done

--- restorent/urls.py
+++ restorent/urls.py
@@ -4,8 +4,9 @@ from . import views
 app_name = "zomotoApp"
 
 urlpatterns = [
-   
     path('menu/add/', views.add_item_to_menu, name='add_item_to_menu'),
+    path('menu/edit/<int:item_id>/', views.edit_item, name='edit_item'),
+    path('menu/remove/<int:item_id>/', views.remove_item, name='remove_item'),
     path('', views.show_menu, name='show_menu'),
     path('order/add/', views.order_item, name='order_item'),
     path('order/update/<int:order_id>/', views.update_order_status, name='update_order_status'),
--- restorent/views.py
+++ restorent/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import JsonResponse
+from django.contrib import messages
+
 
 # Dummy data for demonstration
 menu_items = [
-    {"id": 1, "name": "Burger", "price": 10.99},
-    {"id": 2, "name": "Pizza", "price": 12.99},
+    {"id": 1, "name": "Burger", "price": 10.99, "Desc": "Try once", "availability": True, "quantity": 10},
+    {"id": 2, "name": "Pizza", "price": 12.99, "Desc": "Try once", "availability": True, "quantity": 15},
     # ... more menu items
 ]
+
 
 orders = []
 order_counter = 1
@@ -14,9 +17,9 @@ order_counter = 1
 def add_item_to_menu(request):
     if request.method == "POST":
         data = request.POST  # Replace with your form handling logic
-        new_item = {"id": len(menu_items) + 1, "name": data["name"], "price": float(data["price"])}
+        new_item = {"id": len(menu_items) + 1, "name": data["name"], "price": float(data["price"]),'Desc':data['Desc']}
         menu_items.append(new_item)
-        return JsonResponse({"message": "Item added to menu successfully"})
+        messages.success(request, "Item added to menu successfully")
     return render(request, "add.html")
 
 def show_menu(request):
@@ -34,7 +37,7 @@ def order_item(request):
         }
         orders.append(order)
         order_counter += 1
-        return JsonResponse({"message": "Order placed successfully"})
+        messages.success(request, "Order placed successfully")
     return render(request, "order.html", {"menu_items": menu_items})
 
 def update_order_status(request, order_id):
@@ -43,8 +46,31 @@ def update_order_status(request, order_id):
         for order in orders:
             if order["id"] == int(order_id):
                 order["status"] = data["status"]
-                return JsonResponse({"message": f"Order {order_id} status updated"})
+                messages.success(request, f"Order {order_id} status updated")
     return render(request, "update.html", {"order_id": order_id})
 
 def view_all_orders(request):
     return render(request, "view_order.html", {"orders": orders})
+
+
+def edit_item(request, item_id):
+    if request.method == "POST":
+        data = request.POST  # Replace with your form handling logic
+        for item in menu_items:
+            if item["id"] == int(item_id):
+                item["name"] = data["name"]
+                item["price"] = float(data["price"])
+                item["availability"] = bool(data["availability"])  # Assuming you have an availability checkbox
+                item["quantity"] = int(data["quantity"])
+                messages.success(request, f"Item {item_id} edited successfully")
+    for item in menu_items:
+        if item["id"] == int(item_id):
+            return render(request, "edit_item.html", {"item": item})
+    messages.success(request, "Item not found")
+
+
+def remove_item(request, item_id):
+    global menu_items
+    menu_items = [item for item in menu_items if item["id"] != int(item_id)]
+    messages.success(request, f"Item {item_id} removed successfully")
+    return redirect('zomotoApp:show_menu')
